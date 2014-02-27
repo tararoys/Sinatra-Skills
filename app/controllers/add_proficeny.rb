e1d898b6fb4f3764b16667f0c9b8826c58b1fc2a
Tara Roys <tlroys@gmail.com>
2014-02-27 11:15:20 -0600

    pencils down

--- app/controllers/add_proficeny.rb
+++ app/controllers/add_proficeny.rb
@@ -1,4 +1,20 @@
 get '/add_proficiency' do
+  if session[:user_id] 
+   erb :add_proficiency
+  else
+    "you are not allowed to edit this session"
+  end
 
-  erb :add_proficiency
+end
+
+post '/add_proficiency' do 
+  
+   puts params[:proficiency]
+  #psuedocode 
+  User.where(id: session[user_id]).first << proficiency{params[:proficiency][:skill]}
+
+ 
+
+
+  redirect "/"
 end
